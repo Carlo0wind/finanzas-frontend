2d3332a4c813f54d49fc6b295e2756a49ea44e4a
Carlo050 <u202315055@upc.edu.pe>
2025-12-04 03:21:17 -0500

    adding redirects for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts (PRODUCCIÓN)
 export const environment = {
   production: true,
-  apiUrl: 'https://hominy-g3c4h5g8eza7d8h4.canadacentral-01.azurewebsites.net/api/v1'
+  apiUrl: '/api/v1'
 };
